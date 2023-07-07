c23de4728e8dc3d10aba6ba3b8ae6dd5ff08a727
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-07 16:31:39 +0600

    Folder model

---
+++ src/models/Folder.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+interface FolderType extends Document {
+  name: string;
+  parent: Types.ObjectId | null;
+  children: Types.ObjectId[];
+}
+
+const FolderSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  parent: { type: Schema.Types.ObjectId, ref: "Folder" },
+  children: [{ type: Schema.Types.ObjectId, ref: "Folder" }],
+});
+
+const Folder = mongoose.model<FolderType>("Folder", FolderSchema);
+
+export default Folder;
