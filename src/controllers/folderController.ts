4a37ac3ae47bb462656ab36b980bb427e56082ef
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-09 22:24:25 +0600

    child folder created

--- src/controllers/folderController.ts
+++ src/controllers/folderController.ts
@@ -24,8 +24,18 @@ export const getFolders = async (req: Request, res: Response) => {
 export const createChild = async (req: Request, res: Response) => {
   try {
     const { name, parent } = req.body;
+
     const data = await Child.create({ name, parent });
-    res.json({ message: "Folder created", data: data });
+
+    const id = data._id;
+
+    console.log(id);
+
+    const folder = await Folder.findById(parent);
+
+    await folder?.updateOne({ $push: { children: id } });
+
+    res.json({ message: "Child Folder created", data: data });
   } catch (error) {
     res.json({ status: "error", message: error });
   }
--- src/routes/folderRoute.ts
+++ src/routes/folderRoute.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import { createFolder, getFolders } from "../controllers/folderController";
+import {
+  createChild,
+  createFolder,
+  getFolders,
+} from "../controllers/folderController";
 const router = express.Router();
 
 router.route("/folders").post(createFolder).get(getFolders);
+
+router.route("/child").patch(createChild);
 
 export default router;
