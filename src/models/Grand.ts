9cb59f6f8efabd003d565f2ca97ac0d89e4bcc3b
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-09 22:57:20 +0600

    Grand model

--- src/models/Grand.ts
+++ src/models/Grand.ts
@@ -1,21 +1,21 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
 
-interface ChildType extends Document {
+interface GrandType extends Document {
   name: string;
   parent: Types.ObjectId | null;
   children: Types.ObjectId[] | null;
 }
 
-const ChildSchema: Schema = new Schema({
+const GrandSchema: Schema = new Schema({
   name: { type: String, required: true },
   parent: {
     type: Schema.Types.ObjectId,
-    ref: "Folder",
+    ref: "Child",
     require: true,
   },
-  children: [{ type: Schema.Types.ObjectId, ref: "Grand" }],
+  children: [{ type: Schema.Types.ObjectId, ref: "Child" }],
 });
 
-const Child = mongoose.model<ChildType>("Child", ChildSchema);
+const Grand = mongoose.model<GrandType>("Grand", GrandSchema);
 
-export default Child;
+export default Grand;
